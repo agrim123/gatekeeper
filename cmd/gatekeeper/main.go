81c557239779be55f05715b2d2ad0733a2821d6e
Agrim Mittal <16553016+agrim123@users.noreply.github.com>
2020-11-25 00:22:05 +0530

    (security) Add authentication module

--- cmd/gatekeeper/main.go
+++ cmd/gatekeeper/main.go
@@ -4,10 +4,24 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/agrim123/gatekeeper/internal/pkg/authentication"
+	"github.com/agrim123/gatekeeper/internal/pkg/authorization"
 	"github.com/agrim123/gatekeeper/internal/pkg/root"
 	"github.com/agrim123/gatekeeper/internal/pkg/setup"
 	"github.com/agrim123/gatekeeper/pkg/config"
 )
+
+type GateKeeper struct {
+	AuthenticationModule authentication.Module
+
+	User root.AccessMapping
+}
+
+func NewGatekeeper() *GateKeeper {
+	return &GateKeeper{
+		AuthenticationModule: authentication.NewDefaultModule(),
+	}
+}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -17,6 +31,13 @@ func main() {
 	config.Init()
 
 	setup.Start()
+
+	gatekeeper := NewGatekeeper()
+	if username, authenticated, err := gatekeeper.AuthenticationModule.IsAuthenticated(); !authenticated {
+		panic(err)
+	} else {
+		gatekeeper.User = root.Users[username]
+	}
 
 	plan := os.Args[1]
 
@@ -29,9 +50,19 @@ func main() {
 		return
 	}
 
-	if _, ok := root.Plans[plan].Opts[os.Args[2]]; !ok {
+	option := os.Args[2]
+
+	if _, ok := root.Plans[plan].Opts[option]; !ok {
 		panic("Invalid option")
 	}
 
-	fmt.Println(root.Plans[plan].Opts[os.Args[2]].Run())
+	if authorized, err := authorization.IsAuthorized(gatekeeper.User.User.Email, plan, option); !authorized {
+		panic(err)
+	}
+
+	// fmt.Println(root.Plans[plan].Opts[option].Run())
+}
+
+func (g *GateKeeper) SetAuthenticationModule(authenticationModule authentication.Module) {
+	g.AuthenticationModule = authenticationModule
 }
