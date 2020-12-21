bfc445b0edd4cea647d4b9cb524a31c374a146d7
Agrim Mittal <16553016+agrim123@users.noreply.github.com>
2020-12-21 22:22:00 +0530

    (refactor) Cleanup

--- internal/pkg/utils/utils.go
+++ internal/pkg/utils/utils.go
@@ -22,24 +22,8 @@ func AttachExecutingUserToCtx(ctx context.Context) context.Context {
 	return context.WithValue(ctx, constants.UserContextKey, getExecutingUser())
 }
 
-func AttachPlanToCtx(ctx context.Context, plan string) context.Context {
-	return context.WithValue(ctx, constants.PlanContextKey, plan)
-}
-
-func AttachOptionToCtx(ctx context.Context, option string) context.Context {
-	return context.WithValue(ctx, constants.OptionContextKey, option)
-}
-
 func GetUsernameFromCtx(ctx context.Context) string {
 	return getFromContext(ctx, constants.UserContextKey)
-}
-
-func GetPlanFromCtx(ctx context.Context) string {
-	return getFromContext(ctx, constants.PlanContextKey)
-}
-
-func GetOptionFromCtx(ctx context.Context) string {
-	return getFromContext(ctx, constants.OptionContextKey)
 }
 
 func getExecutingUser() string {
